9fad6dc8322dad014d02d5fc6220283cac119012
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-24 13:20:55 +0200

    done

--- GeometryReaderBC/ContentView.swift
+++ GeometryReaderBC/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    func getPercentage(geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.width / 2
+        let currentX = geo.frame(in: .global).midX
+        return Double(1 - (currentX / maxDistance))
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<20) { index in
+                    GeometryReader { geometry in
+                        RoundedRectangle(cornerRadius: 20).fill(.mint)
+                            .rotation3DEffect(
+                                Angle(degrees: getPercentage(geo: geometry) * 40), axis: (x: 0.0, y: 1.0, z: 0.0))
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                }
+            }
+        }
     }
 }
 
@@ -19,3 +36,20 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+//struct GeometryReaderBC: View {
+//    var body: some View {
+//        GeometryReader { geometry in
+//            HStack(spacing: 0) {
+//                Rectangle()
+//                    .fill(.blue)
+//                    .frame(width: geometry.size.width * 2 / 3)
+//                Rectangle()
+//                    .fill(.yellow)
+//            }
+//        }
+//        .ignoresSafeArea()
+//    }
+//}
