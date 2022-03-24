5f66c4cb33a6e05971ca50564ff87fbc6d14f24d
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-24 13:17:56 +0200

    Initial Commit

---
+++ GeometryReaderBC/GeometryReaderBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderBCApp.swift
+//  GeometryReaderBC
+//
+//  Created by Vahtee Boo on 24.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
